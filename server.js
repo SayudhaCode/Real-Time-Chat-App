f2e37f702028f5515d255c1a15b93f1e9575378e
sayudha <mazaya.studio@outlook.com>
2022-08-25 21:44:01 +0700

    + Basic Routing

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const http = require('http')
 
 const port = process.env.PORT || 1337
 
+function respondText(req, res) {
+    res.setHeader('Content-Type', 'Text/Plain');
+    res.end('Hi');
+}
+
+function respondJson(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({text: 'hi', numbers: [1, 2, 3]}));
+}
+
+function respondNotFound(req, res) {
+    res.writeHead(404, {'Content-Type': 'Text/Plain'})
+    res.end('Not Found')
+}
+
 const server = http.createServer(function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3] }));
+    if (req.url === '/') return respondText(req, res)
+    if (req.url === '/json') return respondJson(req, res)
+
+    respondNotFound(req, res)
 })
 
 server.listen(port)
